696033768e2a3d0a869fbb6959a7bf872cbdefaf
Lisandro Escalada <licha.escalada@gmail.com>
2023-07-13 19:01:57 +0000

    cambios operadores

--- operadores.js
+++ operadores.js
@@ -1,1 +1,20 @@
-console.log(5 % 10);
+//suma
+console.log(3 + 6);
+
+//resta
+console.log(9 - 3);
+
+//multiplicación
+console.log(2 * 12);
+
+//división
+console.log(90 / 9); // bien escrito
+console.log(9 / 90); // mal escrito
+
+//módulo o resto
+console.log(80 % 60);
+console.log(60 % 80); // mal escrito
+
+//precedencia
+console.log((3 + 5) * 2 - 8);
+console.log(3 + 5 * 2 - 8); // mal escrito
--- operadoresDeComparacion.js
+++ operadoresDeComparacion.js
@@ -1,10 +1,18 @@
-console.log(4 < 7);
+/* console.log(4 < 7);
 console.log(4 < 1);
 console.log(4 > 4);
-console.log(4 == 7);
+console.log(4 == 7); */
 
+// Operador de asignacion: "="
+
+// Operador de igualdad débil "==": Compara dos valores para ver si son iguales, realizando una conversión de tipo si es necesario. Esto significa que puede comparar valores de diferentes tipos y tratará de convertirlos antes de hacer la comparación.
+
+// Operador de igualdad estricta "===": Compara dos valores para ver si son iguales, sin realizar ninguna conversión de tipo. Además de comparar el valor, también compara el tipo de datos. Solo devuelve true si los valores son idénticos en tipo y contenido.
+
+// 3 es un dato de tipo number
+// "3" es un dato de tipo string
 
 console.log(3 == 3);
 console.log(3 === 3);
 console.log(3 == '3');
-console.log(3 === '3');
+console.log(3 === '3'); // false xq el de la izq es number y el de la derecha string
