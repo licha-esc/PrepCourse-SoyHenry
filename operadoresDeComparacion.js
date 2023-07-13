be13030a83105ffc8c028985d29d5d114fec0e50
Licha <104387269+licha-esc@users.noreply.github.com>
2023-07-12 21:02:22 -0300

    first commit

---
+++ operadoresDeComparacion.js
@@ -0,0 +1,10 @@
+console.log(4 < 7);
+console.log(4 < 1);
+console.log(4 > 4);
+console.log(4 == 7);
+
+
+console.log(3 == 3);
+console.log(3 === 3);
+console.log(3 == '3');
+console.log(3 === '3');
---
+++ variables.js
@@ -0,0 +1,26 @@
+//* Tipos de datos *
+// String
+var bootcamp = 'Henry';
+
+// Numeros
+var numEntero = 5;
+var numNegativo = -5;
+var numDecimal = 5.5;
+
+// Booleanos
+var estoyEntretenido = true;
+var estoyAburrido = false;
+console.log(estoyEntretenido);
+console.log(estoyAburrido);
+
+// Undefined
+var caja;
+console.log(caja);
+
+// Null
+var sinNada = null;
+console.log(sinNada);
+
+
+//! Método length
+console.log("cuentaCantidadDeLetras".length);
