930d81dd0441e5256262758e5193c7d0f804a2cb
Licha <104387269+licha-esc@users.noreply.github.com>
2024-02-04 04:33:44 -0300

    apuntes

---
+++ apuntes/Apuntes Javascript/JS-04-Funciones/callback.js
@@ -0,0 +1,29 @@
+//! Callback:
+// Proceso en el que una función recibe por parámetro a otra función y la ejecuta en su interior.
+// · Nos aseguran que una función no se va a ejecutar antes de que se complete una tarea, sino que se ejecutará justo después de que la tarea se haya completado.
+
+/* var devuelvoUsuario = function () {
+    return 'Lisandro';
+};
+
+var devuelvoSaludo = function () {
+    return 'Hola';
+};
+
+var saludar = function (cb1, cb2) {
+    return cb1() + " " + cb2();
+};
+
+var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
+console.log(resultado); */
+
+var devuelvoFrase = function(comida) {
+    return 'Hoy quiero comer: ' + comida;
+};
+
+var hablar = function (comida, cb) {
+    return cb(comida);
+};
+
+var fraseFinal = hablar('Pizza', devuelvoFrase);
+console.log(fraseFinal);
